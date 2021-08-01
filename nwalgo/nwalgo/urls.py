79ac0d84f2ebb0904ed9bf2fd19bf2e375dfebe2
fredynai <fredynai@tencent.com>
2021-08-01 21:22:03 +0800

    feat: new action

--- nwalgo/nwalgo/urls.py
+++ nwalgo/nwalgo/urls.py
@@ -19,4 +19,5 @@ from . import views
  
 urlpatterns = [
     path('', views.index),
+    path('runoob', views.runoob)
 ]
--- nwalgo/nwalgo/views.py
+++ nwalgo/nwalgo/views.py
@@ -1,6 +1,12 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
  
 def index(request):
-    context          = {}
+    context = {}
     context['hello'] = 'Hello World!'
     return render(request, 'index.html', context)
+
+def runoob(request):
+    name = request.body
+    print('name',name)
+    return HttpResponse("菜鸟教程")
